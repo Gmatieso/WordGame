c2e38e2341763dd4d6d916ad591193433688e122
Gmatieso <matiesogeoffrey@gmail.com>
2022-02-10 18:12:32 +0300

    added wordGuessApplication file to the AndroidManifest xml file

--- app/src/main/java/com/example/wordguess/wordGuessApplication.kt
+++ app/src/main/java/com/example/wordguess/wordGuessApplication.kt
@@ -1,4 +1,17 @@
 package com.example.wordguess
 
-class wordGuessApplication {
+import android.app.Application
+
+/* Application Class: A base class that contains global application
+case for your entire app. its kinda a main object your OS uses to interact with your main app
+there is always a default application class created by the OS if you not going to do anything special */
+class wordGuessApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        // initialize Timber
+        //Timber.plant(Timber.DebugTree())
+        /* You'll rarely add items in this application class
+        always feel free to use android inbuilt log or timber log
+        */
+    }
 }
